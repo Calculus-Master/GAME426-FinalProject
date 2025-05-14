2e03f805bf18bdff61eb6b02bddc1e60d915b2db
Saptarshi Mallick <sapmallick@gmail.com>
2025-05-13 22:06:46 -0700

    Added placeholder actions for the leisure states

---
+++ Assets/Scripts/FSM/Actions/ObserveDecorationsAction.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Finite State Machine/Actions/Observe Decor")]
+public class ObserveDecorationsAction : FSMAction
+{
+    public override void Act(FiniteStateMachine fsm)
+    {
+        // TODO: Insert behavior tree here
+    }
+}
---
+++ Assets/Scripts/FSM/Actions/SocializeAction.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Finite State Machine/Actions/Socialize")]
+public class SocializeAction : FSMAction
+{
+    public override void Act(FiniteStateMachine fsm)
+    {
+        // TODO: Insert behavior tree here
+    }
+}
